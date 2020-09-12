5f57805d5c1aeeb247c93a06a6506fc9a127d4ac
Graham Vasquez <rescus1221@gmail.com>
2020-09-12 13:57:22 -0400

    🎨 Manually ran prettier on existing files

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const morgan = require('morgan');
 
 app.use(express.json());
 if (process.env.NODE_ENV !== 'production') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    //eslint-disable-next-line
-    console.log(`Server started on port ${PORT}`);
+  //eslint-disable-next-line
+  console.log(`Server started on port ${PORT}`);
 });
