065c5915a3112e854bf727a4b66219fc11a852d1
Graham Vasquez <rescus1221@gmail.com>
2020-09-12 15:08:45 -0400

    ✅ Initialized tests

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  //eslint-disable-next-line
+  console.log(`Server started on port ${PORT}`);
+});
