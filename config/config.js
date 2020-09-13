14a0d545486dfa642154492c024575dae85daba9
Sean Metzgar <sean.metzgar@gmail.com>
2020-09-13 03:49:10 -0400

    🔧 Update sequelize config file

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_TEST_USERNAME,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_DATABASE,
+    host: process.env.DB_TEST_HOSTNAME,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+  },
+};
