f35247378417d3941b33961e66f541fe7fdbda64
Graham Vasquez <rescus1221@gmail.com>
2020-09-19 03:09:11 -0400

    ➖ Removed passport

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const passport = require('passport');
 const cookieParser = require('cookie-parser');
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json());
@@ -13,8 +12,6 @@ if (process.env.NODE_ENV !== 'production') {
 app.get('/', (req, res) => {
   return res.json({ data: 'Welcome to this survey app!' });
 });
-
-app.use(passport.initialize());
 
 app.use('/api/v1', require('./routes/v1/v1-index'));
 
