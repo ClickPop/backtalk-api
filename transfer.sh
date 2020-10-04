5c6e1b5f447d7f6b56917e9e9dbb7ba8805928d2
Graham Vasquez <rescus1221@gmail.com>
2020-10-03 23:48:27 -0400

    🔨 Made some more changes to the deploy scripts
    Added some more .env data to handle the difference between staging and master.

--- transfer.sh
+++ transfer.sh
@@ -1,7 +1,15 @@
 if [[ $CIRCLE_BRANCH == 'staging' ]]; then
-  echo PORT=5001 >> .env
+  echo -e "PORT=5001\n
+  NODE_ENV=production\n
+  API_NAME=backtalk-api-staging\n
+  DB_NAME=backtalk-db-staging\n
+  DB_LOCATION='../data-staging'" >> .env
   rsync -va --delete ./ circleci@api.backtalk.io:~/backtalk-staging
 elif [[ $CIRCL_BRANCH == 'master' ]]; then
-  echo PORT=5000 >> .env
+  echo -e "PORT=5000\n
+  NODE_ENV=production\n
+  API_NAME=backtalk-api\n
+  DB_NAME=backtalk-db\n
+  DB_LOCATION='../data'" >> .env
   rsync -va --delete ./ circleci@api.backtalk.io:~/backtalk-api
 fi
