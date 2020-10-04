6720f860991b1450040ac1922b151c4c7e9dc39d
Graham Vasquez <rescus1221@gmail.com>
2020-10-04 01:33:29 -0400

    🔨 Fixing small issue with DB_HOST

--- transfer.sh
+++ transfer.sh
@@ -3,7 +3,6 @@
 echo -e "DB_USERNAME=$DB_USERNAME\n
 DB_PASSWORD=$DB_PASSWORD\n
 DB_DATABASE=$DB_DATABASE\n
-DB_HOST=backtalk-db\n
 HASH_SECRET=$HASH_SECRET\n
 ACCESS_TOKEN_SECRET=$ACCESS_TOKEN_SECRET\n
 REFRESH_TOKEN_SECRET=$REFRESH_TOKEN_SECRET\n
@@ -14,6 +13,7 @@ REFRESH_TOKEN_EXPIRATION=$REFRESH_TOKEN_EXPIRATION\n" > .env
 # Handle whether we are on the staging or master branch
 if [[ $CIRCLE_BRANCH == 'staging' ]]; then
   echo -e "PORT=5001\n
+DB_HOST=backtalk-db-staging\n
 NODE_ENV=production\n
 API_NAME=backtalk-api-staging\n
 DB_NAME=backtalk-db-staging\n
@@ -22,6 +22,7 @@ DB_LOCATION=../data-staging" >> .env
   rsync -va --delete ./ circleci@api.backtalk.io:~/backtalk-staging
 elif [[ $CIRCLE_BRANCH == 'master' ]]; then
   echo -e "PORT=5000\n
+DB_HOST=backtalk-db\n
 NODE_ENV=production\n
 API_NAME=backtalk-api\n
 DB_NAME=backtalk-db\n
