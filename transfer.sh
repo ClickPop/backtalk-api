7483d4d79bc834f36aa7eb4131f359cc55c9334b
Graham Vasquez <rescus1221@gmail.com>
2020-10-04 00:02:31 -0400

    🔨 Removed single quotes from the DB_LOCATION env declaration to attempt to fix a bug.

--- transfer.sh
+++ transfer.sh
@@ -3,13 +3,13 @@ if [[ $CIRCLE_BRANCH == 'staging' ]]; then
   NODE_ENV=production\n
   API_NAME=backtalk-api-staging\n
   DB_NAME=backtalk-db-staging\n
-  DB_LOCATION='../data-staging'" >> .env
+  DB_LOCATION=../data-staging" >> .env
   rsync -va --delete ./ circleci@api.backtalk.io:~/backtalk-staging
 elif [[ $CIRCL_BRANCH == 'master' ]]; then
   echo -e "PORT=5000\n
   NODE_ENV=production\n
   API_NAME=backtalk-api\n
   DB_NAME=backtalk-db\n
-  DB_LOCATION='../data'" >> .env
+  DB_LOCATION=../data" >> .env
   rsync -va --delete ./ circleci@api.backtalk.io:~/backtalk-api
 fi
