c331fff1ce89ca41a5d62d21997c5a9ed25d673d
Sean Metzgar <sean.metzgar@gmail.com>
2020-11-15 02:21:19 -0500

    🐛 Fixes issue with empty geo data on responses

--- models/response.js
+++ models/response.js
@@ -15,16 +15,19 @@ module.exports = (sequelize, DataTypes) => {
       ipAddressFormatted: {
         type: DataTypes.VIRTUAL,
         get() {
-          const rawValue = this.ipAddress;
-          let tempIP = ip6addr.parse(rawValue);
-          let ipv4 = tempIP.toString({ format: 'v4' });
-          let ipv6 = tempIP.toString({ format: 'v6' });
-          let returnValue = { raw: rawValue };
-          if (ipv4) {
-            returnValue.ipv4 = ipv4;
-          }
-          if (ipv6) {
-            returnValue.ipv6 = ipv6;
+          let returnValue = null;
+          if (this.ipAddress) {
+            let rawIP = this.ipAddress;
+            let tempIP = ip6addr.parse(rawIP);
+            let ipv4 = tempIP.toString({ format: 'v4' });
+            let ipv6 = tempIP.toString({ format: 'v6' });
+            returnValue = { raw: rawIP };
+            if (ipv4) {
+              returnValue.ipv4 = ipv4;
+            }
+            if (ipv6) {
+              returnValue.ipv6 = ipv6;
+            }
           }
           return returnValue;
         },
@@ -39,10 +42,32 @@ module.exports = (sequelize, DataTypes) => {
         get() {
           let rVal = null;
           if (
+            this.ipAddressFormatted !== null &&
             typeof this.ipAddressFormatted === 'object' &&
             'ipv4' in this.ipAddressFormatted
           ) {
             rVal = geoip.lookup(this.ipAddressFormatted.ipv4);
+
+            if (rVal === null || rVal === undefined) {
+              let cidr1 = ip6addr.createCIDR('10.0.0.0', 8);
+              let cidr2 = ip6addr.createCIDR('172.16.0.0', 12);
+              let cidr3 = ip6addr.createCIDR('192.168.0.0', 16);
+              let ipv4 = this.ipAddressFormatted.ipv4;
+
+              if ('127.0.0.1' === ipv4) {
+                rVal = { type: 'localhost' };
+              } else if (
+                cidr1.contains(ipv4) ||
+                cidr2.contains(ipv4) ||
+                cidr3.contains(ipv4)
+              ) {
+                rVal = { type: 'private' };
+              } else {
+                rVal = { type: 'unknown' };
+              }
+            } else if (typeof rVal === 'object' && rVal !== null) {
+              rVal.type = 'location';
+            }
           }
           return rVal;
         },
