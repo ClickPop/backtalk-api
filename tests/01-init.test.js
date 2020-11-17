d0205129cc7fca3562e188cfbb129fcb5cc04a97
Sean Metzgar <sean.metzgar@gmail.com>
2020-11-17 13:30:52 -0500

    👽 Add api details to root response

--- tests/01-init.test.js
+++ tests/01-init.test.js
@@ -13,7 +13,7 @@ describe('Initial Endpoint', () => {
   it('should respond at api root', async (done) => {
     const res = await req.get('/').set('User-Agent', uaString);
     expect(res.status).toBe(200);
-    expect(res.body.data).toBe('Welcome to this survey app!');
+    expect(res.body.data).toBe('Welcome to the Backtalk API');
     done();
   });
 });
