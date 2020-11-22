f862309bc9c1d8e7da929b6b9d9fa7ce4b59970a
Graham Vasquez <rescus1221@gmail.com>
2020-11-22 17:09:43 -0500

    ✨ Added the ability to update survey info.
    ✅ Added tests for survey update route.
    ✨ Added survey info to response route to handle displaying it on the front end without making a second API call.

--- tests/03-v1-surveys.test.js
+++ tests/03-v1-surveys.test.js
@@ -207,6 +207,58 @@ describe('Surveys', () => {
     });
   });
 
+  describe('Update survey', () => {
+    it('should respond with a 200 if the survey is updated.', async (done) => {
+      const res = await req
+        .patch('/api/v1/surveys/update')
+        .set('Authorization', `Bearer ${commonInfo.accessToken}`)
+        .set('User-Agent', uaString)
+        .send({
+          surveyId: commonInfo.firstSurvey.id,
+          title: 'Updated Title',
+          description: 'This is a test description',
+          questions: [
+            {
+              prompt: 'This is question 1 prompt',
+              description: 'This is question 1 description',
+              type: 'text',
+            },
+            {
+              prompt: 'This is question 2 prompt',
+              description: 'This is question 2 description',
+              type: 'text',
+            },
+          ],
+        });
+      expect(res.status).toBe(201);
+      expect(res.body.result.title).toBe('Updated Title');
+      expect(res.body.result.description).toBe('This is a test description');
+      expect(res.body.result.questions).toHaveLength(2);
+      res.body.result.questions.forEach((question) => {
+        expect(question).toHaveProperty('prompt');
+        expect(question).toHaveProperty('description');
+        expect(question).toHaveProperty('type');
+      });
+      done();
+    });
+
+    it('should respond with a 401 if the user is not logged in.', async (done) => {
+      const res = await req
+        .patch('/api/v1/surveys/update')
+        .set('User-Agent', uaString)
+        .send({ surveyId: commonInfo.firstSurvey.id });
+      expect(res.status).toBe(401);
+      expect(res.body.errors).toEqual(
+        expect.arrayContaining([
+          {
+            msg: 'Unauthorized',
+          },
+        ]),
+      );
+      done();
+    });
+  });
+
   describe('Delete survey', () => {
     it('should respond with a 200 if the survey is deleted.', async (done) => {
       const res = await req
