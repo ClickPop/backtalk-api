d578cbfed2a1545af86c63f01144cf81bf4295a1
Graham Vasquez <rescus1221@gmail.com>
2021-08-21 15:33:04 -0400

    Moved from serverless to firebase

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const app = require('./server/app');
-const serverless = require('serverless-http');
-
+const functions = require('firebase-functions');
 const PORT = process.env.PORT || 5000;
 
-module.exports.handler = serverless(app);
+exports.expressApi = functions.runWith({ memory: '1GB' }).https.onRequest(app);
 
-app.listen(PORT, () => {
-  //eslint-disable-next-line
-  console.log(`Server started on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'prod') {
+  app.listen(PORT, () => {
+    //eslint-disable-next-line
+    console.log(`Server started on port ${PORT}`);
+  });
+}
