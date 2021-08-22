cb6d1ec56de4df285978bf6f4db7e42fa1a35931
Graham Vasquez <rescus1221@gmail.com>
2021-08-22 00:09:39 -0400

    Change env for server listen

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 exports.expressApi = functions.runWith({ memory: '1GB' }).https.onRequest(app);
 
-if (process.env.NODE_ENV !== 'prod') {
+if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
     //eslint-disable-next-line
     console.log(`Server started on port ${PORT}`);
