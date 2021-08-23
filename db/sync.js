05b83ac8b87d13ce163f7bad209010e3c7326783
Graham Vasquez <rescus1221@gmail.com>
2021-08-23 01:58:28 -0400

    Removed the DB sync every time the server starts to try and reduce cold start times

--- db/sync.js
+++ db/sync.js
@@ -4,12 +4,14 @@ const { Question } = require('../models/question');
 const { Response } = require('../models/response');
 const { Role } = require('../models/role');
 const { Survey } = require('../models/survey');
+
+User.associate(sequelize.models);
+Role.associate(sequelize.models);
+Survey.associate(sequelize.models);
+Question.associate(sequelize.models);
+Response.associate(sequelize.models);
+
 module.exports = async () => {
-  User.associate(sequelize.models);
-  Role.associate(sequelize.models);
-  Survey.associate(sequelize.models);
-  Question.associate(sequelize.models);
-  Response.associate(sequelize.models);
   await sequelize.sync();
   // eslint-disable-next-line no-console
   console.info('Sequelize Synced');
